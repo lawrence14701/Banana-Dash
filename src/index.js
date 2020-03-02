584d820579ce2a1f98112540aa4a54dc8d322e6b
lawrence_nic <your_email_here>
2020-03-02 11:42:23 -0500

    npm install new dependecies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import './styles/index.scss';
 
+const canvas = document.getElementById("myCanvas");
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
 
-// const canvas =  document.getElementById('myCanvas');
-// const context = canvas.getContext('2d');
+const ctx = canvas.getContext("2d"); //magic paint brush that creates 2d elements
 
-// draw(context){
-//     context.fillStyle = "#0ff"
-//     context.fillRect(10,10,50,50)
-// }
+ctx.fillRect(50,50,50,50);
+ctx.fillRect(505, 50, 50, 50);
+ctx.fillRect(50, 50, 50, 50);
+ctx.fillRect(50, 50, 50, 50);
 
-console.log('testing')
