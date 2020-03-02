e101d8fd8ef4ebde8a30880c688d2111d6fb6885
lawrence_nic <your_email_here>
2020-03-02 09:46:02 -0500

    webpack config

--- js/main.js
+++
@@ -1,3 +0,0 @@
-const canvas =  document.getElementById('myCanvas');
-const context = canvas.getContext('2d');
-context.fillRect(0,0,50,50)
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './styles/index.scss';
+
+
+
+// const canvas =  document.getElementById('myCanvas');
+// const context = canvas.getContext('2d');
+
+// draw(context){
+//     context.fillStyle = "#0ff"
+//     context.fillRect(10,10,50,50)
+// }
+
+console.log('testing')
