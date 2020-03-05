f08d96717bb6f9b6afb59289edd64cde955561af
lawrence_nic <your_email_here>
2020-03-04 23:05:36 -0500

    add jumping to monkey

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-import './styles/index.scss';
+import "./styles/index.scss";
 
-const canvas = document.getElementById('myCanvas');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
 
-
-const IDLE_MONKEY_PATH = '/src/img/idle'
-const NUMBER_OF_IDLE_IMAGES = 17;
 const DURATION = 1000;
-const TIME_PER_FRAME = DURATION/NUMBER_OF_IDLE_IMAGES;
+const IDLE_MONKEY_PATH = "/src/img/idle";
+const NUMBER_OF_IDLE_IMAGES = 17;
+const TIME_PER_IDLE_FRAME = DURATION / NUMBER_OF_IDLE_IMAGES;
+
+const JUMPING_MONKEY_PATH = "/src/img/jumping";
+const NUMBER_OF_JUMPING_IMAGES = 5;
+const TIME_PER_JUMPING_FRAME = DURATION / NUMBER_OF_JUMPING_IMAGES;
 
 var timeWhenLastUpdate;
 var timeFromLastUpdate;
-var frameNumber = 0;
-
-
+var frameNumberIdle = 0;
+var frameNumberJumping = 0;
 
 var monkey = {
-    idle: new Image(),
-    jumping: true,
-    width: 100,
-    height: 100,
-    x_velocity: 0,
-    y_velocity: 0,
-    x: 100,
-    y: 0,
-
-}
-
+  idle: new Image(),
+  isIdle: true,
+  jumping: false,
+  width: 100,
+  height: 100,
+  x_velocity: 0,
+  y_velocity: 0,
+  x: 100,
+  y: 0
+};
 
 const controller = {
   left: false,
@@ -49,45 +50,68 @@ const controller = {
   }
 };
 
-
-
 ///////////////////
 //IDLE Monkey//////
 //////////////////
-
 
 // function step(startTime){
 
 // }
 
 function step(startTime) {
-// context.fillStyle='white';
-// context.fillRect(0,0,canvas.width,canvas.height);
+  // context.fillStyle='white';
+  // context.fillRect(0,0,canvas.width,canvas.height);
+  if (monkey.isIdle) {
+    if (!timeWhenLastUpdate) {
+      timeWhenLastUpdate = startTime;
+    }
+    timeFromLastUpdate = startTime - timeWhenLastUpdate;
+    if (timeFromLastUpdate > TIME_PER_IDLE_FRAME) {
+      monkey.idle.src = IDLE_MONKEY_PATH + "/idle_" + frameNumberIdle + ".png";
+      context.drawImage(
+        monkey.idle,
+        monkey.x,
+        monkey.y,
+        monkey.width,
+        monkey.height
+      );
 
-  if (!timeWhenLastUpdate) {
-    timeWhenLastUpdate = startTime;
-  }
-  timeFromLastUpdate = startTime - timeWhenLastUpdate;
-  if (timeFromLastUpdate > TIME_PER_FRAME) {
-    monkey.idle.src = IDLE_MONKEY_PATH + "/idle_" + frameNumber + ".png";
-    context.drawImage(
-      monkey.idle,
-      monkey.x,
-      monkey.y,
-      monkey.width,
-      monkey.height
-    );
-
-    timeWhenLastUpdate = startTime;
-    if (frameNumber >= NUMBER_OF_IDLE_IMAGES) {
-      frameNumber = 0;
-    } else {
-      frameNumber++;
+      timeWhenLastUpdate = startTime;
+      if (frameNumberIdle >= NUMBER_OF_IDLE_IMAGES) {
+        frameNumberIdle = 0;
+      } else {
+        frameNumberIdle++;
+      }
     }
   }
+    if (monkey.jumping) {
+      if (!timeWhenLastUpdate) {
+        timeWhenLastUpdate = startTime;
+      }
+      timeFromLastUpdate = startTime - timeWhenLastUpdate;
+      if (timeFromLastUpdate > TIME_PER_IDLE_FRAME) {
+        monkey.idle.src =
+          JUMPING_MONKEY_PATH + "/jumping_" + frameNumberJumping + ".png";
+        context.drawImage(
+          monkey.idle,
+          monkey.x,
+          monkey.y,
+          monkey.width,
+          monkey.height
+        );
+
+        timeWhenLastUpdate = startTime;
+        if (frameNumberJumping >= NUMBER_OF_JUMPING_IMAGES) {
+          frameNumberJumping = 0;
+        } else {
+          frameNumberJumping++;
+        }
+      }
+    }
 
   if (controller.up && monkey.jumping == false) {
     monkey.y_velocity -= 20;
+    monkey.isIdle = false
     monkey.jumping = true;
   }
 
@@ -105,26 +129,14 @@ function step(startTime) {
   monkey.x_velocity *= 0.9; // friction
   monkey.y_velocity *= 0.9; // friction
 
-
-    if (monkey.y > canvas.height - 16 - monkey.height) {
-      monkey.jumping = false;
-      monkey.y = canvas.height - 16 - monkey.height;
-      monkey.y_velocity = 0;
-    }
+  if (monkey.y > canvas.height - 16 - monkey.height) {
+    monkey.isIdle = true;
+    monkey.jumping = false;
+    monkey.y = canvas.height - 16 - monkey.height;
+    monkey.y_velocity = 0;
+  }
   requestAnimationFrame(step);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 window.addEventListener("keydown", controller.keyListener);
 window.addEventListener("keyup", controller.keyListener);
