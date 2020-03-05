0209dc0a55c0f43b3c293b185fb58879ab09c865
lawrence_nic <your_email_here>
2020-03-04 22:29:27 -0500

    add controls to monkey

--- src/index.js
+++ src/index.js
@@ -1,103 +1,8 @@
 import './styles/index.scss';
 
-// var canvas
-// var context
-// var ballX = 75;
-// var ballY = 75;
-// var ballSpeedX = 5;
-// var ballSpeedY = 5;
+const canvas = document.getElementById('myCanvas');
+const context = canvas.getContext('2d');
 
-// const PADDLE_WIDTH = 100;
-// const PADDLE_THICKNESS = 10;
-// const PADDLE_DIST_FROM_EDGE = 60; //10% height of canvas
-// var paddleX = 400;
-
-// function updateMousePos(evt){
-//     var rect = canvas.getBoundingClientRect();
-//     var root = document.documentElement
-//     var mouseX = evt.clientX - rect.left - root.scrollLeft;
-//     // var mouseY = evt.clientY - rect.top - root.scrollTop;
-//     paddleX = mouseX - PADDLE_WIDTH/2;
-// }
-
-
-// window.onload = function () {
-//     canvas = document.getElementById('myCanvas')
-//     context = canvas.getContext('2d');
-//     var framesPerSecond = 30;
-//     setInterval(updateAll, 1000/framesPerSecond); 
-
-//     canvas.addEventListener('mousemove', updateMousePos)
-// }
-
-
-// function updateAll(){
-//     moveAll();
-//     drawAll();
-// }
-
-// function ballReset(){
-//     ballX = canvas.width/2
-//     ballY = canvas.height/2;
-// }
-
-// function moveAll(){
-//     ballX+= ballSpeedX
-//     ballY += ballSpeedY
-//     if(ballX > canvas.width){
-//         ballSpeedX *= -1;
-//     }
-//     if (ballX < 0) {
-//     ballSpeedX *= -1;
-//     }
-//     if (ballY < 0) {
-//       ballSpeedY *= -1;
-//     }
-//     if(ballY > canvas.height){
-//         ballReset()
-//     }
-
-//     var paddleTopEdgeY = canvas.height - PADDLE_DIST_FROM_EDGE;
-//     var paddleBottomEdgeY = paddleTopEdgeY + PADDLE_THICKNESS
-//     var paddleLeftEdgeX = paddleX
-//     var paddleRightEdgeX = paddleTopEdgeY + PADDLE_THICKNESS;
-// }
-
-
-
-// function drawAll(){
-//     colorRect(0, 0, canvas.width, canvas.height, 'black');
-//     colorCircle(ballX, ballY, 10, 'white');
-//     colorRect(paddleX, canvas.height - PADDLE_DIST_FROM_EDGE,PADDLE_WIDTH,PADDLE_THICKNESS,'white')
-// }
-
-
-
-
-
-
-
-
-// //////////////////////////////
-// //draw ball and background////
-// /////////////////////////////
-// function colorRect(topLeftX, topLeftY,boxWidth, boxHeight, fillColor){
-//      context.fillStyle = fillColor;
-//      context.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
-// }
-
-// function colorCircle(centerX, centerY, radius, fillColor){
-//       context.fillStyle = fillColor;
-//       context.beginPath();
-//       context.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
-//       context.fill();
-// }
-
-
-
-
-// const canvas = document.getElementById('myCanvas');
-// const context = canvas.getcon
 
 const IDLE_MONKEY_PATH = '/src/img/idle'
 const NUMBER_OF_IDLE_IMAGES = 17;
@@ -110,27 +15,118 @@ var frameNumber = 0;
 
 
 
+var monkey = {
+    idle: new Image(),
+    jumping: true,
+    width: 100,
+    height: 100,
+    x_velocity: 0,
+    y_velocity: 0,
+    x: 100,
+    y: 0,
 
-const monkey = document.getElementById('idle-monkey');
-
-
-function step(startTime){
-    if(!timeWhenLastUpdate){
-        timeWhenLastUpdate = startTime;
-    }
-    timeFromLastUpdate =  startTime - timeWhenLastUpdate;
-    if (timeFromLastUpdate > TIME_PER_FRAME){
-        monkey.src = IDLE_MONKEY_PATH + '/idle_' + frameNumber + '.png';
-        timeWhenLastUpdate = startTime;
-        if (frameNumber >= NUMBER_OF_IDLE_IMAGES){
-            frameNumber = 0;
-        } else {
-            frameNumber++;
-
-        }
-    }
-    requestAnimationFrame(step);
 }
 
 
-requestAnimationFrame(step);
+const controller = {
+  left: false,
+  right: false,
+  up: false,
+  keyListener: function(event) {
+    var key_state = event.type == "keydown" ? true : false;
+
+    switch (event.keyCode) {
+      case 37: // left key
+        controller.left = key_state;
+        break;
+      case 38: // up key
+        controller.up = key_state;
+        break;
+      case 39: // right key
+        controller.right = key_state;
+        break;
+    }
+  }
+};
+
+
+
+///////////////////
+//IDLE Monkey//////
+//////////////////
+
+
+// function step(startTime){
+
+// }
+
+function step(startTime) {
+// context.fillStyle='white';
+// context.fillRect(0,0,canvas.width,canvas.height);
+
+  if (!timeWhenLastUpdate) {
+    timeWhenLastUpdate = startTime;
+  }
+  timeFromLastUpdate = startTime - timeWhenLastUpdate;
+  if (timeFromLastUpdate > TIME_PER_FRAME) {
+    monkey.idle.src = IDLE_MONKEY_PATH + "/idle_" + frameNumber + ".png";
+    context.drawImage(
+      monkey.idle,
+      monkey.x,
+      monkey.y,
+      monkey.width,
+      monkey.height
+    );
+
+    timeWhenLastUpdate = startTime;
+    if (frameNumber >= NUMBER_OF_IDLE_IMAGES) {
+      frameNumber = 0;
+    } else {
+      frameNumber++;
+    }
+  }
+
+  if (controller.up && monkey.jumping == false) {
+    monkey.y_velocity -= 20;
+    monkey.jumping = true;
+  }
+
+  if (controller.left) {
+    monkey.x_velocity -= 0.5;
+  }
+
+  if (controller.right) {
+    monkey.x_velocity += 0.5;
+  }
+
+  monkey.y_velocity += 1.5; // gravity
+  monkey.x += monkey.x_velocity;
+  monkey.y += monkey.y_velocity;
+  monkey.x_velocity *= 0.9; // friction
+  monkey.y_velocity *= 0.9; // friction
+
+
+    if (monkey.y > canvas.height - 16 - monkey.height) {
+      monkey.jumping = false;
+      monkey.y = canvas.height - 16 - monkey.height;
+      monkey.y_velocity = 0;
+    }
+  requestAnimationFrame(step);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("keydown", controller.keyListener);
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(step);
+// window.requestAnimationFrame(loop);
