510a890620297c97ccffa77da346f57c534da0f5
lawrence_nic <your_email_here>
2020-03-06 14:23:50 -0500

    mergining

--- src/index.js
+++ src/index.js
@@ -1,391 +1,290 @@
-// import "./styles/index.scss";
+import "./styles/index.scss";
 
-var canvas = document.getElementById("game");
-var context = canvas.getContext('2d');
-// var gameStarted = false;
-// var keys = [];
-// var friction = 0.8;
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
 
-// var player = {
-// 	x: 5,
-// 	y: canvas.height - 20,
-// 	width: 20,
-// 	height: 20,
-// 	speed: 5,
-// 	velX: 0,
-// 	velY: 0,
-// 	color: "#ff0000",
-// 	draw: function(){
-// 		context.fillStyle = this.color;
-// 		context.fillRect(this.x, this.y, this.width, this.height);
-// 	}
-// }
+const DURATION_IDLE = 200;
+const DURATION_JUMPING = 100;
+const DURATION_RUNNING = 200;
 
-// document.body.addEventListener("keydown", function(event){
+const IDLE_MONKEY_PATH = "/src/img/idle";
+const JUMPING_MONKEY_PATH = "/src/img/jumping";
+const RUNNING_MONKEY_PATH = "/src/img/running";
 
-// 	if(event.keyCode == 13 && !gameStarted){
-// 		startGame();
-// 	}
-// 	keys[event.keyCode] = true;
+const NUMBER_OF_IDLE_IMAGES = 17;
+const NUMBER_OF_JUMPING_IMAGES = 4;
+const NUMBER_OF_RUNNING_IMAGES = 13;
 
-// });
+// const TIME_PER_IDLE_FRAME = DURATION_IDLE / NUMBER_OF_IDLE_IMAGES;
+// const TIME_PER_JUMPING_FRAME = DURATION_JUMPING / NUMBER_OF_JUMPING_IMAGES;
+// const TIME_PER_RUNNING_FRAME = DURATION_RUNNING / NUMBER_OF_RUNNING_IMAGES;
 
-// document.body.addEventListener("keyup", function(event){
-// 	keys[event.keyCode] = false;
-// });
+var timeWhenLastUpdate;
+var timeFromLastUpdate;
+var frameNumberIdle = 0;
+var frameNumberJumping = 0;
+var frameNumberRunning = 0;
+var key_state;
 
-// intro_screen();
+var monkey = {
+  image: new Image(),
+  isIdle: true,
+  jumping: false,
+  running: false,
+  width: 100,
+  height: 100,
+  x_velocity: 0,
+  y_velocity: 0,
+  x: 100,
+  y: 0
+};
 
-// function intro_screen(){
-// 	context.font = "50px Impact";
-// 	context.fillStyle = "#0099CC";
-// 	context.textAlign = "center";
-// 	context.fillText("HTML5 Game", canvas.width/2, canvas.height/2);
+const controller = {
+  left: false,
+  right: false,
+  up: false,
+  keyListener: function(event) {
+    key_state = event.type == "keydown" ? true : false;
 
-// 	context.font = "20px Arial";
-// 	context.fillText("Press Enter To Start", canvas.width/2, canvas.height/2 + 50);
-// }
+    switch (event.keyCode) {
+      case 37: // left key
+        controller.left = key_state;
+        break;
+      case 38: // up key
+        controller.up = key_state;
+        break;
+      case 39: // right key
+        controller.right = key_state;
+        break;
+    }
+  }
+};
 
-// function startGame(){
-// 	gameStarted = true;
-// 	clearCanvas();
+function step(startTime) {
+  const backgroundImage = new Image();
+  backgroundImage.src = "/src/img/background/background_1.png";
+  context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+  renderLevel(monkey);
 
-// 	setInterval(function(){
-// 		clearCanvas();
-// 		loop();
-// 	}, 1000/30)
-// }
+  if (monkey.isIdle) {
+    if (!timeWhenLastUpdate) {
+      timeWhenLastUpdate = startTime;
+    }
+    timeFromLastUpdate = startTime - timeWhenLastUpdate;
+    // if (timeFromLastUpdate > TIME_PER_IDLE_FRAME) {
+    monkey.image.src = IDLE_MONKEY_PATH + "/idle_" + frameNumberIdle + ".png";
+    context.drawImage(
+      monkey.image,
+      monkey.x,
+      monkey.y,
+      monkey.width,
+      monkey.height
+    );
 
-// function loop(){
+    timeWhenLastUpdate = startTime;
+    if (frameNumberIdle >= NUMBER_OF_IDLE_IMAGES) {
+      frameNumberIdle = 0;
+    } else {
+      frameNumberIdle++;
+    }
+    // }
+  }
+  if (monkey.jumping) {
+    if (!timeWhenLastUpdate) {
+      timeWhenLastUpdate = startTime;
+    }
+    timeFromLastUpdate = startTime - timeWhenLastUpdate;
+    //   if (timeFromLastUpdate > TIME_PER_JUMPING_FRAME) {
+    monkey.image.src =
+      JUMPING_MONKEY_PATH + "/jumping_" + frameNumberJumping + ".png";
+    context.drawImage(
+      monkey.image,
+      monkey.x,
+      monkey.y,
+      monkey.width,
+      monkey.height,
+    );
 
-// 	player.draw();
+    timeWhenLastUpdate = startTime;
+    if (frameNumberJumping >= NUMBER_OF_JUMPING_IMAGES) {
+      frameNumberJumping = 0;
+    } else {
+      frameNumberJumping++;
+    }
+    //   }
+  }
 
-// 	if(keys[39]){
-// 		if(player.velX < player.speed){
-// 			player.velX++;
-// 		}
-// 	}
+  if (monkey.running) {
+    if (!timeWhenLastUpdate) {
+      timeWhenLastUpdate = startTime;
+    }
+    timeFromLastUpdate = startTime - timeWhenLastUpdate;
+    //   if (timeFromLastUpdate > TIME_PER_RUNNING_FRAME) {
+    monkey.image.src =
+      RUNNING_MONKEY_PATH + "/running_" + frameNumberRunning + ".png";
+    context.drawImage(
+      monkey.image,
+      monkey.x,
+      monkey.y,
+      monkey.width,
+      monkey.height
+    );
 
-// 	if(keys[37]){
-// 		if(player.velX > -player.speed){
-// 			player.velX--;
-// 		}
-// 	}
+    timeWhenLastUpdate = startTime;
+    if (frameNumberRunning >= NUMBER_OF_RUNNING_IMAGES) {
+      frameNumberRunning = 0;
+    } else {
+      frameNumberRunning++;
+    }
+    //   }
+  }
+  if (controller.up && monkey.jumping == false) {
+    monkey.y_velocity -= 30;
+    monkey.isIdle = false;
+    monkey.running = false;
+    monkey.jumping = true;
+    monkey.ground = false
+  }
 
-// 	player.x += player.velX;
-// 	player.velX *= friction;
+  if (controller.left) {
+    monkey.x_velocity -= 0.5;
+  }
 
-// }
+  if (controller.right) {
+    monkey.isIdle = false;
+    monkey.running = true;
+    monkey.x_velocity += 0.5;
+  }
 
-// function clearCanvas(){
-// 	context.clearRect(0, 0, 640, 360);
-// }
+  monkey.y_velocity += 1.5; // gravity
+  monkey.x += monkey.x_velocity;
+  monkey.y += monkey.y_velocity;
+  monkey.x_velocity *= 0.9; // friction
+  monkey.y_velocity *= 0.9; // friction
 
-// const DURATION_IDLE = 200;
-// const DURATION_JUMPING = 100;
-// const DURATION_RUNNING = 200;
+  if (monkey.y > canvas.height - monkey.height) {
+    monkey.jumping = false;
+    monkey.y = canvas.height - monkey.height;
+    monkey.y_velocity = 0;
+  }
 
-// const IDLE_MONKEY_PATH = "/src/img/idle";
-// const JUMPING_MONKEY_PATH = "/src/img/jumping";
-// const RUNNING_MONKEY_PATH = "/src/img/running";
+  if (!key_state && !monkey.jumping) {
+    monkey.isIdle = true; //this is probably in the wrong spot
+    monkey.running = false;
+  }
+  // if(key_state){
+  //     monkey.isIdle = false
+  // }
 
-// const NUMBER_OF_IDLE_IMAGES = 17;
-// const NUMBER_OF_JUMPING_IMAGES = 4;
-// const NUMBER_OF_RUNNING_IMAGES = 13;
+  requestAnimationFrame(step);
+}
 
-// // const TIME_PER_IDLE_FRAME = DURATION_IDLE / NUMBER_OF_IDLE_IMAGES;
-// // const TIME_PER_JUMPING_FRAME = DURATION_JUMPING / NUMBER_OF_JUMPING_IMAGES;
-// // const TIME_PER_RUNNING_FRAME = DURATION_RUNNING / NUMBER_OF_RUNNING_IMAGES;
+// window.requestAnimationFrame(loop);
 
-// var timeWhenLastUpdate;
-// var timeFromLastUpdate;
-// var frameNumberIdle = 0;
-// var frameNumberJumping = 0;
-// var frameNumberRunning = 0;
-// var key_state;
+///////////////////////////////////////////////tiles and levels////////////////////////////////
 
-// var monkey = {
-//   image: new Image(),
-//   isIdle: true,
-//   jumping: false,
-//   running: false,
-//   width: 100,
-//   height: 100,
-//   x_velocity: 0,
-//   y_velocity: 0,
-//   x: 100,
-//   y: 0,
-//   cropX: 15,
-//   cropY: 15,
-// };
+const levelArray = [
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "                    ",
+  "               GR   ",
+  "                   W",
+  "                  WW",
+  "                 WWW",
+  "GGGGGGGGGGGGGGGGGGGG",
+  "DDDDDDDDDDDDDDDDDDDD"
+];
 
-// const controller = {
-//   left: false,
-//   right: false,
-//   up: false,
-//   keyListener: function(event) {
-//     key_state = event.type == "keydown" ? true : false;
+class Tile {
+  constructor(width, height) {
+    this.path = "./src/img/tiles/";
+    this.width = width;
+    this.height = height;
+  }
+  draw(context, tilePath, x, y) {
+    let image = new Image();
+    image.src = this.path + tilePath;
+    context.drawImage(image, x, y, this.width, this.height);
+  }
+}
 
-//     switch (event.keyCode) {
-//       case 37: // left key
-//         controller.left = key_state;
-//         break;
-//       case 38: // up key
-//         controller.up = key_state;
-//         break;
-//       case 39: // right key
-//         controller.right = key_state;
-//         break;
-//     }
-//   }
-// };
+function rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2){
+    // Check x and y for overlap
+    if (x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2){
+        return false;
+    }
+    return true;
+};
 
-// function step(startTime) {
-//   const backgroundImage = new Image();
-//   backgroundImage.src = "/src/img/background/background_1.png";
-//   context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-//   renderLevel(monkey);
 
-//   if (monkey.isIdle) {
-//     if (!timeWhenLastUpdate) {
-//       timeWhenLastUpdate = startTime;
-//     }
-//     timeFromLastUpdate = startTime - timeWhenLastUpdate;
-//     // if (timeFromLastUpdate > TIME_PER_IDLE_FRAME) {
-//     monkey.image.src = IDLE_MONKEY_PATH + "/idle_" + frameNumberIdle + ".png";
-//     context.drawImage(
-//       monkey.image,
-//       monkey.x,
-//       monkey.y,
-//       monkey.width,
-//       monkey.height
-//     );
 
-//     timeWhenLastUpdate = startTime;
-//     if (frameNumberIdle >= NUMBER_OF_IDLE_IMAGES) {
-//       frameNumberIdle = 0;
-//     } else {
-//       frameNumberIdle++;
-//     }
-//     // }
-//   }
-//   if (monkey.jumping) {
-//     if (!timeWhenLastUpdate) {
-//       timeWhenLastUpdate = startTime;
-//     }
-//     timeFromLastUpdate = startTime - timeWhenLastUpdate;
-//     //   if (timeFromLastUpdate > TIME_PER_JUMPING_FRAME) {
-//     monkey.image.src =
-//       JUMPING_MONKEY_PATH + "/jumping_" + frameNumberJumping + ".png";
-//     context.drawImage(
-//       monkey.image,
-//       monkey.x,
-//       monkey.y,
-//       monkey.width,
-//       monkey.height,
-//     );
+function renderLevel(monkey) {
+  
+  const grass = "grass.png";
+  const dirt = 'dirt.png'
+  const woodenBox = 'wooden_box.png'
+  const platformEdgeRight = 'platform_right.png'
+  const tile = new Tile(50, 50);
 
-//     timeWhenLastUpdate = startTime;
-//     if (frameNumberJumping >= NUMBER_OF_JUMPING_IMAGES) {
-//       frameNumberJumping = 0;
-//     } else {
-//       frameNumberJumping++;
-//     }
-//     //   }
-//   }
+  let x = 0;
+  let y = 0;
 
-//   if (monkey.running) {
-//     if (!timeWhenLastUpdate) {
-//       timeWhenLastUpdate = startTime;
-//     }
-//     timeFromLastUpdate = startTime - timeWhenLastUpdate;
-//     //   if (timeFromLastUpdate > TIME_PER_RUNNING_FRAME) {
-//     monkey.image.src =
-//       RUNNING_MONKEY_PATH + "/running_" + frameNumberRunning + ".png";
-//     context.drawImage(
-//       monkey.image,
-//       monkey.x,
-//       monkey.y,
-//       monkey.width,
-//       monkey.height
-//     );
+  for (let row = 0; row < levelArray.length; row++) {
+    let tileRow = levelArray[row]
+   for (let col = 0; col < tileRow.length; col++) {
+     let singleTile = tileRow[col];
+     if(singleTile === 'G'){
+       tile.draw(context,grass,x,y)
+       if (rectIntersect(x, y, 25, 25, monkey.x, monkey.y, monkey.width, monkey.height)){
+          monkey.y = y - monkey.height
+          monkey.jumping = false;
+          monkey.y_velocity = 0;
 
-//     timeWhenLastUpdate = startTime;
-//     if (frameNumberRunning >= NUMBER_OF_RUNNING_IMAGES) {
-//       frameNumberRunning = 0;
-//     } else {
-//       frameNumberRunning++;
-//     }
-//     //   }
-//   }
-//   if (controller.up && monkey.jumping == false) {
-//     monkey.y_velocity -= 30;
-//     monkey.isIdle = false;
-//     monkey.running = false;
-//     monkey.jumping = true;
-//     monkey.ground = false
-//   }
+       }
+     } else if(singleTile === 'D'){
+        tile.draw(context, dirt, x, y);
+      if (rectIntersect(x, y, 25, 25, monkey.x, monkey.y, monkey.width, monkey.height)){
+          monkey.y = y - monkey.height
+          monkey.jumping = false;
+          monkey.y_velocity = 0;
 
-//   if (controller.left) {
-//     monkey.x_velocity -= 0.5;
-//   }
+       } 
+       else if(singleTile === 'R'){
+        tile.draw(context, platformEdgeRight, x, y);
+      // if (rectIntersect(x, y, 25, 25, monkey.x, monkey.y, monkey.width, monkey.height)){
+      //     monkey.y = y - monkey.height
+      //     monkey.jumping = false;
+      //     monkey.y_velocity = 0;
 
-//   if (controller.right) {
-//     monkey.isIdle = false;
-//     monkey.running = true;
-//     monkey.x_velocity += 0.5;
-//   }
+       }
+     } else if (singleTile === 'W'){
+       tile.draw(context,woodenBox,x,y)
+       if (rectIntersect(x, y, 25, 25, monkey.x, monkey.y, monkey.width, monkey.height)){
+          monkey.y = y - monkey.height
+          monkey.jumping = false;
+          monkey.y_velocity = 0;
 
-//   monkey.y_velocity += 1.5; // gravity
-//   monkey.x += monkey.x_velocity;
-//   monkey.y += monkey.y_velocity;
-//   monkey.x_velocity *= 0.9; // friction
-//   monkey.y_velocity *= 0.9; // friction
+       }
+     }
+     x += 50
+   }
+    y += 40
+    x = 0
+  }
+  
+}
 
-//   if (monkey.y > canvas.height - monkey.height) {
-//     monkey.jumping = false;
-//     monkey.y = canvas.height - monkey.height;
-//     monkey.y_velocity = 0;
-//   }
-
-//   if (!key_state && !monkey.jumping) {
-//     monkey.isIdle = true; //this is probably in the wrong spot
-//     monkey.running = false;
-//   }
-//   // if(key_state){
-//   //     monkey.isIdle = false
-//   // }
-
-//   requestAnimationFrame(step);
-// }
-
-// // window.requestAnimationFrame(loop);
-
-// ///////////////////////////////////////////////tiles and levels////////////////////////////////
-
-// const levelArray = [
-//   "                    ",
-//   "                    ",
-//   "                    ",
-//   "                    ",
-//   "                    ",
-//   "                    ",
-//   "                    ",
-//   "                    ",
-//   "                    ",
-//   "                    ",
-//   "                    ",
-//   "      B             ",
-//   "      W             ",
-//   "      W   B         ",
-//   "      GGGGGGGGGGG   ",
-//   "                   W",
-//   "                  WW",
-//   "                 WWW",
-//   "GGGGGGGGGGGGGGGGGGGG",
-//   "DDDDDDDDDDDDDDDDDDDD"
-// ];
-
-// class Tile {
-//   constructor(width, height) {
-//     this.path = "./src/img/tiles/";
-//     this.width = width;
-//     this.height = height;
-//   }
-//   draw(context, tilePath, x, y) {
-//     let image = new Image();
-//     image.src = this.path + tilePath;
-//     context.drawImage(image, x, y, this.width, this.height);
-//   }
-// }
-
-// function rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2){
-//     // Check x and y for overlap
-//     if (x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2){
-//         return false;
-//     }
-//     return true;
-// };
-
-// function renderLevel(monkey) {
-
-//   const grass = "grass.png";
-//   const dirt = 'dirt.png'
-//   const woodenBox = 'wooden_box.png'
-//   const platformEdgeRight = 'platform_right.png'
-//   const banana = 'banana.png'
-//   const tile = new Tile(50, 50);
-
-//   let x = 0;
-//   let y = 0;
-
-//   for (let row = 0; row < levelArray.length; row++) {
-//     let tileRow = levelArray[row]
-//    for (let col = 0; col < tileRow.length; col++) {
-//      let singleTile = tileRow[col];
-//      if(singleTile === 'G'){
-//        tile.draw(context,grass,x,y)
-//        if (rectIntersect(x, y, 25, 25, monkey.x, monkey.y, monkey.width, monkey.height)){
-//           monkey.y = y - monkey.height
-//           monkey.jumping = false;
-//           monkey.y_velocity = 0;
-//           // monkey.x = x - 25;
-
-//        }
-//      } else if(singleTile === 'D'){
-//         tile.draw(context, dirt, x, y);
-//       if (rectIntersect(x, y, 25, 25, monkey.x, monkey.y, monkey.width, monkey.height)){
-//           monkey.y = y - monkey.height
-//           monkey.jumping = false;
-//           monkey.y_velocity = 0;
-
-//        }
-//        else if(singleTile === 'B'){
-//          debugger
-//         tile.draw(context, banana, 10, 10);
-//       if (rectIntersect(x, y, 25, 25, monkey.x, monkey.y, monkey.width, monkey.height)){
-//           monkey.y = y - monkey.height
-//           monkey.jumping = false;
-//           monkey.y_velocity = 0;
-
-//        }
-//       }
-//        else if(singleTile === 'R'){
-//         tile.draw(context, platformEdgeRight, x, y);
-//       // if (rectIntersect(x, y, 25, 25, monkey.x, monkey.y, monkey.width, monkey.height)){
-//       //     monkey.y = y - monkey.height
-//       //     monkey.jumping = false;
-//       //     monkey.y_velocity = 0;
-
-//        }
-//      } else if (singleTile === 'W'){
-//        tile.draw(context,woodenBox,x,y)
-//        if (rectIntersect(x, y, 25, 25, monkey.x, monkey.y, monkey.width, monkey.height)){
-//           monkey.y = y - monkey.height
-//           monkey.jumping = false;
-//           monkey.y_velocity = 0;
-
-//        }
-//      }
-//      x += 50
-//    }
-//     y += 40
-//     x = 0
-//   }
-
-// }
-
-// window.addEventListener("keydown", controller.keyListener);
-// window.addEventListener("keyup", controller.keyListener);
-// window.requestAnimationFrame(step);
-
-// let simpleLevelPlan = `
-// ......................
-// ..#................#..
-// ..#..............=.#..
-// ..#.........o.o....#..
-// ..#.@......#####...#..
-// ..#####............#..
-// ......#++++++++++++#..
-// ......##############..
-// ......................`;
+window.addEventListener("keydown", controller.keyListener);
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(step);
