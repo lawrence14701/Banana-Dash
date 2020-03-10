ce333c14110fd325540859ce5fbbf7b8b482e5e4
lawrence_nic <your_email_here>
2020-03-10 10:09:11 -0400

    move collision algo in another file

---
+++ src/collisionCheck.js
@@ -0,0 +1,37 @@
+export function collisionCheck(character, platform) {
+  //vector is distance between both the character and the platform
+  var vectorX =
+    character.x + character.width / 2 - (platform.x + platform.width / 2);
+  var vectorY =
+    character.y + character.height / 2 - (platform.y + platform.height / 2);
+
+  var halfWidths = character.width / 2 + platform.width / 2;
+  var halfHeights = character.height / 2 + platform.height / 2;
+
+  var collisionDirection = null;
+
+  //checking if the vector is less than halfwidth AKA collision
+  if (Math.abs(vectorX) < halfWidths && Math.abs(vectorY) < halfHeights) {
+    var offsetX = halfWidths - Math.abs(vectorX);
+    var offsetY = halfHeights - Math.abs(vectorY);
+    if (offsetX < offsetY) {
+      if (vectorX > 0) {
+        collisionDirection = "left";
+        character.x += offsetX;
+      } else {
+        collisionDirection = "right";
+        character.x -= offsetX;
+      }
+    } else {
+      if (vectorY > 0) {
+        collisionDirection = "top";
+        character.y += offsetY;
+      } else {
+        collisionDirection = "bottom";
+        character.y -= offsetY;
+      }
+    }
+  }
+
+  return collisionDirection;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles/index.scss";
 import { intro_screen } from './intro';
 import Platforms from './platforms';
+import {collisionCheck} from './collisionCheck';
 
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
@@ -41,41 +42,7 @@ var goal = {
   }
 }
 
-function collisionCheck(character, platform) {
-  //vector is distance between both the character and the platform
-  var vectorX = character.x + character.width / 2 - (platform.x + platform.width / 2); 
-  var vectorY =character.y + character.height / 2 - (platform.y + platform.height / 2);
 
-  var halfWidths = character.width / 2 + platform.width / 2;
-  var halfHeights = character.height / 2 + platform.height / 2;
-
-  var collisionDirection = null;
-
-  //checking if the vector is less than halfwidth AKA collision
-  if (Math.abs(vectorX) < halfWidths && Math.abs(vectorY) < halfHeights) {
-    var offsetX = halfWidths - Math.abs(vectorX);
-    var offsetY = halfHeights - Math.abs(vectorY);
-    if (offsetX < offsetY) {
-      if (vectorX > 0) {
-        collisionDirection = "left";
-        character.x += offsetX;
-      } else {
-        collisionDirection = "right";
-        character.x -= offsetX;
-      }
-    } else {
-      if (vectorY > 0) {
-        collisionDirection = "top";
-        character.y += offsetY;
-      } else {
-        collisionDirection = "bottom";
-        character.y -= offsetY;
-      }
-    }
-  }
-
-  return collisionDirection;
-}
 /////////////////IWANTTODELETE///////////////////////
 
 
