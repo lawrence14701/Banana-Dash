69c2a78bb81d919883d1ed873bc8cfaf09011d4d
lawrence_nic <your_email_here>
2020-03-20 17:52:44 -0400

    add enivronment

--- src/levels.js
+++ src/levels.js
@@ -13,10 +13,10 @@ class LevelMaker {
     this.levelsIndex = 0;
     this.levels = [
       [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], //1
+        [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8], //1
         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6], //2
         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6], //3
-        [7, 'B', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6], //4
+        [7, 'B', 11, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6], //4
         [7, 4, 3, 3, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6], //5
         [7, 0, 0, 0, 0, 0, 4, 3, 5, 0, 0, 0, 0, 0, 0, 6], //6
         [7, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 5, 0, 0, 0, 6], //7
@@ -24,13 +24,13 @@ class LevelMaker {
         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 5, 0, 6], //9
         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 6], //10
         [7, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 6], //11
-        [7, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 6], //12
+        [7, 0, 0, 0, 0, 9, 0, 2, 2, 2, 2, 0, 0, 0, 0, 6], //12
         [7, 0, 0, 4, 3, 3, 3, 3, 3, 3, 3, 5, 0, 0, 0, 6], //13
-        [7, "P", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6], //14
+        [7, "P", 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 6], //14
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] //15
       ],
       [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], //1
+        [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8], //1
         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6], //2
         [7, 0, 0, 0, 0, 0, 'B', 0, 0, 0, 0, 0, 0, 0, 0, 6], //3
         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6], //4
@@ -55,8 +55,8 @@ class LevelMaker {
   }
 
   draw(x, y, width, height, url) {
-    let image = new Image();
-    image.src = "/src/img/" + url;
+    let image = document.getElementById(url)
+    debugger
     this.context.drawImage(image, x, y, width, height);
   }
 
@@ -90,28 +90,32 @@ class LevelMaker {
       for (let j = 0; j < this.levelCols; j++) {
         let x = j * this.tileSize;
         let y = i * this.tileSize;
+        let position = level[i][j]
         //platforms
-        if (level[i][j] === 1) this.pushAndDraw(x,y,'tiles/grass.png')
-        if (level[i][j] === 2) this.pushAndDraw(x,y,'tiles/wooden_box.png') 
-        if (level[i][j] === 4) this.pushAndDraw(x,y,'tiles/platform_edge_left.png')
-        if (level[i][j] === 3) this.pushAndDraw(x,y,'tiles/platform.png')
-        if (level[i][j] === 5) this.pushAndDraw(x,y,'tiles/platform_edge_right.png')
-        if (level[i][j] === 6) this.pushAndDraw(x,y,'tiles/right.png')
-        if (level[i][j] === 7) this.pushAndDraw(x,y,'tiles/left.png')
-        if (level[i][j] === "B") {
-          this.platforms.bananas.push({ //this is pushing to bananas array
+        if (position === 1) this.pushAndDraw(x,y,'grass')
+        if (position === 2) this.pushAndDraw(x,y,'woodenBox') 
+        if (position === 4) this.pushAndDraw(x,y,'platformEdgeLeft')
+        if (position === 3) this.pushAndDraw(x,y,'platform')
+        if (position === 5) this.pushAndDraw(x,y,'platformEdgeRight')
+        if (position === 6) this.pushAndDraw(x,y,'right')
+        if (position === 7) this.pushAndDraw(x,y,'left')
+        if (position === 8) this.pushAndDraw(x, y, "dirt");
+
+        
+
+        //environment
+        if(position === 9) this.draw(x,y,50,60,'bush')
+        if (position === 11) this.draw(x, y, 60, 70, "flag");
+
+
+        if (position === "B") {
+          this.platforms.bananas.push({ //this is pushing to bananas array of platform hash
             x: x,
             y: y,
             width: this.tileSize,
             height: this.tileSize
           });
-          this.draw(
-            x,
-            y,
-            this.tileSize,
-            this.tileSize,
-            "collectable/banana.png"
-          );
+          this.draw(x, y, this.tileSize, this.tileSize, "banana");
         }
       }
     }
