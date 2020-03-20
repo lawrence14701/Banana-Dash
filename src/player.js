b0e55b3a06c707076a706ae63c09f3d57f0e60a1
lawrence_nic <your_email_here>
2020-03-19 22:23:43 -0400

    monkey animation smoothed out

--- src/player.js
+++ src/player.js
@@ -13,136 +13,121 @@ export default class Player {
 
     this.jumping = false;
     this.grounded = false;
-    this.running = false
+    this.running = false;
 
-    this.monkey = new Image();
+    this.monkey; // = new Image();
     //idle
-    this.idleMonkeyPath = "/src/img/idle/Idle_";
     this.durationIdle = 200;
     this.numberOfIdleImages = 17;
     this.timePerIdleFrame = this.durationIdle / this.numberOfIdleImages;
+    this.frameNumberIdle = 0;
     //running
-    this.RunningMonkeyPath = "/src/img/running/Running_";
     this.durationRunning = 200;
     this.numberOfRunningImages = 14;
-    this.timePerRunningFrame = this.durationRunning / this.numberOfRunningImages;
+    this.timePerRunningFrame =
+    this.durationRunning / this.numberOfRunningImages;
+    this.frameNumberRunning = 0;
+    //jumping
+    this.durationJumping = 200;
+    this.numberOfJumpingImages = 5;
+    this.timePerJumpingFrame =
+    this.durationJumping / this.numberOfJumpingImages;
+    this.frameNumberJumping = 0;
 
+    //animation variables
     this.timeWhenLastUpdate = null;
     this.timeFromLastUpdate = null;
-    this.frameNumberIdle = 0;
-    this.frameNumberJumping = 0;
-    this.frameNumberRunning = 0;
-    this.started = false
+    this.started = false;
     this.startTime = 0;
-
-  //    for (var i = 0; i < this.numberOfIdleImages; i++) {
-  //   document.body.append("<div id='preload-image-'"+i+"' style='background-image: url("+this.idleMonkeyPath+i +".png);'></div>");
-  // }
-
-    // const DURATION_JUMPING = 100;
-    // const DURATION_RUNNING = 200;
-
-    // const JUMPING_MONKEY_PATH = "/src/img/jumping";
-    // const RUNNING_MONKEY_PATH = "/src/img/running";
-
-    // const NUMBER_OF_JUMPING_IMAGES = 4;
-    // const NUMBER_OF_RUNNING_IMAGES = 13;
-
-    // const TIME_PER_JUMPING_FRAME = DURATION_JUMPING / NUMBER_OF_JUMPING_IMAGES;
-    // const TIME_PER_RUNNING_FRAME = DURATION_RUNNING / NUMBER_OF_RUNNING_IMAGES;
   }
   draw() {
-      this.startTime = performance.now();
-      if(this.jumping) this.jumping();
-      else if(this.running) this.runningAnimation();
-      else this.idle();
+    this.startTime = performance.now();
+    if (this.jumping) this.jumpingAnimation();
+    else if (this.running) this.runningAnimation();
+    else this.idleAnimation();
   }
 
-  idle() {
-    this.monkey.src = this.idleMonkeyPath + this.frameNumberIdle + ".png";
-    this.context.fillRect(this.x,this.y,this.width,this.height);
-    // this.context.drawImage(this.monkey,this.x, this.y, this.width, this.height);
-    this.context.drawImage(this.monkey,0,0,60,60,this.x, this.y, this.width, this.height);
+  idleAnimation() {
+    this.monkey = document.getElementById("idle_" + this.frameNumberIdle);
+    this.context.drawImage(
+      this.monkey,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
 
-    if (!this.timeWhenLastUpdate) {  //initial picture that we render (starting point) //draw is called 16 ms
+    if (!this.timeWhenLastUpdate) {
+      //initial picture that we render (starting point) //draw is called 16 ms
       this.timeWhenLastUpdate = this.startTime - this.timePerIdleFrame;
     }
-    // console.log(this.startTime + '||' + this.timeWhenLastUpdate);
     this.timeFromLastUpdate = this.startTime - this.timeWhenLastUpdate;
-      // console.log(this.timeFromLastUpdate + '>' + this.timePerIdleFrame + '||' +(this.timeFromLastUpdate > this.timePerIdleFrame));
 
-    if (this.timeFromLastUpdate > this.timePerIdleFrame) { //how long the image renders until we render the new one
-      this.monkey.src = this.idleMonkeyPath + this.frameNumberIdle + ".png";
-      // this.context.drawImage(this.monkey, this.x, this.y, this.width, this.height);
-
+    if (this.timeFromLastUpdate > this.timePerIdleFrame) {
+      //how long the image renders until we render the new one
+      this.monkey = document.getElementById("idle_" + this.frameNumberIdle);
       this.timeWhenLastUpdate = this.startTime;
-      if (this.frameNumberIdle >= this.numberOfIdleImages) {
-        this.frameNumberIdle = 0;
-      } else {
+      if (this.frameNumberIdle < this.numberOfIdleImages - 1) {
         this.frameNumberIdle++;
+      } else {
+        this.frameNumberIdle = 0;
       }
     }
   }
 
   runningAnimation() {
-    this.monkey.src = this.RunningMonkeyPath + this.frameNumberRunning + ".png";
-    this.context.fillRect(this.x, this.y, this.width, this.height);
-    this.context.drawImage(this.monkey, this.x, this.y, this.width, this.height);
+    this.monkey = document.getElementById("running_" + this.frameNumberRunning);
+    this.context.drawImage(
+      this.monkey,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+
     if (!this.timeWhenLastUpdate) {
-      //initial picture that we render (starting point) //draw is called 16 ms
+      //initial picture that we render (starting point)
       this.timeWhenLastUpdate = this.startTime - this.timePerIdleFrame;
     }
-    // console.log(this.startTime + '||' + this.timeWhenLastUpdate);
     this.timeFromLastUpdate = this.startTime - this.timeWhenLastUpdate;
-    // console.log(this.timeFromLastUpdate + '>' + this.timePerIdleFrame + '||' +(this.timeFromLastUpdate > this.timePerIdleFrame));
 
-    if (this.timeFromLastUpdate > this.timePerIdleFrame) {
+    if (this.timeFromLastUpdate > this.timePerRunningFrame) {
       //how long the image renders until we render the new one
-      this.monkey.src = this.RunningMonkeyPath + this.frameNumberRunning + ".png";
-      // this.context.drawImage(
-      //   this.monkey,
-      //   this.x,
-      //   this.y,
-      //   this.width,
-      //   this.height
-      // );
-
+      this.monkey = document.getElementById("idle_" + this.frameNumberRunning);
       this.timeWhenLastUpdate = this.startTime;
-      if (this.frameNumberRunning >= this.numberOfRunningImages) {
-        this.frameNumberRunning = 0;
-      } else {
+      if (this.frameNumberRunning < this.numberOfRunningImages - 1) {
         this.frameNumberRunning++;
+      } else {
+        this.frameNumberRunning = 0;
       }
     }
   }
 
-  jumping() {}
+  jumpingAnimation() {
+        this.monkey = document.getElementById("jumping_" + this.frameNumberJumping);
+        this.context.drawImage(
+          this.monkey,
+          this.x,
+          this.y,
+          this.width,
+          this.height
+        );
+
+        if (!this.timeWhenLastUpdate) {
+          //initial picture that we render (starting point) //draw is called 16 ms
+          this.timeWhenLastUpdate = this.startTime - this.timePerJumpingFrame;
+        }
+        this.timeFromLastUpdate = this.startTime - this.timeWhenLastUpdate;
+
+        if (this.timeFromLastUpdate > this.timePerJumpingFrame) {
+          //how long the image renders until we render the new one
+          this.monkey = document.getElementById("jumping_" + this.frameNumberJumping);
+          this.timeWhenLastUpdate = this.startTime;
+          if (this.frameNumberJumping < this.numberOfJumpingImages - 1) { //we minus one bec we have to account for the 0th index
+            this.frameNumberJumping++;
+          } else {
+            this.frameNumberJumping = 0;
+          }
+        }
+  }
 }
-
-// function step(startTime) {
-//   if (!this.timeWhenLastUpdate) {
-//     this.timeWhenLastUpdate = startTime;
-//   }
-//   this.timeFromLastUpdate = startTime - this.timeWhenLastUpdate;
-
-//   if (this.timeFromLastUpdate > this.timePerIdleFrame) {
-//     this.monkey.src = this.idleMonkeyPath + "/idle_" + this.frameNumberIdle + ".png";
-//     context.drawImage(
-//       monkey,
-//       this.x,
-//       this.y,
-//       this.width,
-//       this.height
-//     );
-
-//     this.timeWhenLastUpdate = startTime;
-//     if (frameNumberIdle >= this.numberOfIdleImages) {
-//       this.frameNumberIdle = 0;
-//     } else {
-//       this.frameNumberIdle++;
-//     }
-//   }
-//   requestAnimationFrame(step)
-// }
-
-// window.requestAnimationFrame(step)
