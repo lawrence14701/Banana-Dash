fdc23c55175ef40c2e1440b244a8476e8cf56112
lawrence_nic <your_email_here>
2020-03-20 23:19:08 -0400

    refactor collision detection

--- src/collisionCheck.js
+++ src/collisionCheck.js
@@ -1,12 +1,12 @@
 export function collisionCheck(character, platform) {
   //vector is distance between both the character and the platform
   var vectorX =
-    character.x + character.width / 3 - (platform.x + platform.width / 2); // divided character by 3 bec of white space
+    (character.x + character.width / 2) - (platform.x + platform.width / 2); 
   var vectorY =
     character.y + character.height / 2 - (platform.y + platform.height / 2);
 
-  var halfWidths = character.width / 3 + platform.width / 2; // i divided by 3 instead of 2 to account for the whitespace of pixels around my image
-  var halfHeights = character.height / 2 + platform.height / 2;
+  var halfWidths = character.width / 3.2 + platform.width / 2; // i divided by 3 instead of 2 to account for the whitespace of pixels around my image
+  var halfHeights = character.height / 2 + platform.height / 2; // i wanted to have a smaller representation of the characters width
 
   var collisionDirection = null;
 
